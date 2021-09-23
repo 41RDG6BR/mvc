3a12183681dea9a3222e112e1d4bfcfbc4050541
41RDG6BR <rdg6design@gmail.com>
2021-09-23 14:25:17 -0300

    Displaying Cart Items on the Cart Page

--- models/cart.js
+++ models/cart.js
@@ -20,11 +20,11 @@ module.exports = class Cart {
             )
             
             const existingProduct = cart.products[existingProductIndex]
-
+                
             let updatedProduct;
 
             if(existingProduct) {
-                updatedproduct = {...existingProduct}
+                updatedProduct = {...existingProduct}
                 updatedProduct.qty = updatedProduct.qty + 1
                 cart.products = [...cart.products]
                 cart.products[existingProductIndex] = updatedProduct
@@ -32,10 +32,41 @@ module.exports = class Cart {
                 updatedProduct = { id: id, qty : 1}
                 cart.products = [...cart.products, updatedProduct]
             }
-            cart.totalPrice = cart.totalPrice + productPrice
+            cart.totalPrice = cart.totalPrice + +productPrice
             fs.writeFile(p, JSON.stringify(cart), (err) => {
                 console.log(err)
             })
+        })
+    }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if(err) {
+                return;
+            }
+            const updatedCart = { ...JSON.parse(fileContent) };
+            const product = updatedCart.products.find(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(
+                prod => prod.id !== id
+            )
+            updatedCart.totalPrice = 
+                updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+                console.log(err)
+            })
+        })
+    }
+
+    static getProducts(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart= JSON.parse(fileContent);
+            if(err) {
+                cb(null)
+            } else {
+                cb(cart)
+            }
         })
     }
 }
