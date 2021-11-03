d430921f4b725d5faf7e03035c1779e9c27369ed
41RDG6BR <rdg6design@gmail.com>
2021-11-03 14:34:05 -0300

    Understanding Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'esteveaqui#RDG6'
+const sequelize = new Sequelize('node-complete', 'root', 'esteveaqui#RDG6', { 
+  dialect: 'mysql', 
+  host: 'localhost' 
 });
 
-module.exports = pool.promise();
+module.exports = sequelize
