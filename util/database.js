71bb64a1fb71c19d7e1e1e97d6814a477060b1de
Rodrigo Nogueira <rdg6design@gmail.com>
2022-09-19 01:27:24 -0300

    Working with NoSQL & Using MongoDB

--- util/database.js
+++ util/database.js
@@ -1,9 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', { 
-  dialect: 'mysql', 
-  host: 'localhost' 
-});
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://javascriptmastery:@cluster01.ybjeb.mongodb.net/?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected!')
+      callback(client)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
-module.exports = sequelize
-//teste
+module.exports = mongoConnect;
